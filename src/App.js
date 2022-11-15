fd40f7fe1e12a1a30075f62e70f0f50970420c7c
Emily LaBelle <emilylabelle8@gmail.com>
2022-11-15 14:43:22 -0500

    added multiple languges

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import { useState } from 'react';
+import { useState, createContext  } from 'react';
+import Details from './components/Details';
 import English from './lang/en.json'
 import Spanish from './lang/es.json'
+import French from './lang/fr.json'
 import './App.css';
 
 const locale = navigator.language || 'en';
 
-const _lang = (locale === 'es') ? Spanish : English;
+const _lang = (locale === 'es') 
+? Spanish
+: (locale === 'fr')
+? French
+ : English;
+
+export const LanguageContext = createContext()
 
 function App() {
   const [lang, setLang] = useState(_lang)
   return (
+    <LanguageContext.Provider value={{lang}}>
     <div className="App">
       <header className="App-header">
         <div>
           <button onClick={() => setLang(English)}>EN</button>
           <button onClick={() => setLang(Spanish)}>ES</button>
+          <button onClick={() => setLang(French)}>FR</button>
         </div>
         <h1>{lang["app.header"]}</h1>
         <p>{lang["app.subhead"]}</p>
@@ -26,8 +36,10 @@ function App() {
         >
           {lang["app.cta"]}
         </a>
+        <Details />
       </header>
     </div>
+    </LanguageContext.Provider>
   );
 }
 
